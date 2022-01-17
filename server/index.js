fadb8138700ee6423c904d9f5f3bd96c051fb98f
Bogdan R <bogdan20226@gmail.com>
2022-01-17 15:31:39 +0200

    fixAuth

--- server/index.js
+++ server/index.js
@@ -1,23 +1,21 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
 
-const db = require('./db')
-const movieRouter = require('./routes/movie-router')
+const db = require('./db');
 
-const app = express()
-const apiPort = 3000
+const movieRouter = require('./routes/movie-router');
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
-app.use(bodyParser.json())
+const app = express();
+const port = process.env.PORT || 3000;
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-app.use('/api', movieRouter)
+app.use('/api', movieRouter);
 
-app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+app.listen(port, () => console.log(`Server running on port ${port}`));
